3f7fabd3cc4192e485284ed17f8421e782e02231
melchor629 <melchor9000@gmail.com>
2014-05-03 23:37:17 +0200

    that shouldn't be there...

--- lastSongs.comp.js
+++ lastSongs.comp.js
@@ -475,31 +475,6 @@
     };
 })(window, jQuery);
 
-
-//build-script.js
-var fs = require('fs');
-var js = 'æ';
-
-if(fs.existsSync('lastSongs.comp.js'))
-    fs.unlinkSync('lastSongs.comp.js');
-
-var files = fs.readdirSync('.');
-for(var i = 0; i < files.length; i++) {
-    var file = files[i];
-    if(file.substr(-2) !== 'js')
-        continue;
-    js += '//' + file + '\n';
-    js += fs.readFileSync(file, { encoding: 'UTF-8' });
-    js += '\n\n';
-}
-
-fs.writeFile('lastSongs.comp.js', js, {
-    encoding: 'UTF-8'
-}, function(err) {
-    if(err) throw err;
-    console.log("Files concatenated correctly");
-});
-
 //jquery.slimscroll.min.js
 /*! Copyright (c) 2011 Piotr Rochala (http://rocha.la)
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
